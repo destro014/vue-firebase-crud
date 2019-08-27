81ce1d16be4c837c0a54a2dd669b6818141c01d0
Destro <destro014@users.noreply.github.com>
2019-08-27 19:21:27 +0545

    remove api key

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -4,13 +4,12 @@ import firestore from 'firebase/firestore'
 // Initialize Firebase
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAP48LDE-oP8jHLov-ZwzEVqFok7LJ0ny0",
+  apiKey: "",
   authDomain: "vue-firebase-crud-e1355.firebaseapp.com",
   databaseURL: "https://vue-firebase-crud-e1355.firebaseio.com",
   projectId: "vue-firebase-crud-e1355",
   storageBucket: "vue-firebase-crud-e1355.appspot.com",
-  messagingSenderId: "648276205911",
-  // appId: "1:648276205911:web:fb2fd8726200df00"
+  messagingSenderId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
